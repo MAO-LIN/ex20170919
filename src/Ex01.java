2d92429a577e56381944127622b15b0edc4f95a3
SamaLin <105021058@live.asia.edu.tw>
2017-09-19 12:43:25 +0800

    20170919

--- src/Ex01.java
+++ src/Ex01.java
@@ -1,6 +1,6 @@
 public class Ex01 {
     public static void main(String[] args){
-        MainFrame mfr = nesw MainFrame();
+        MainFrame mfr = new  MainFrame();
         mfr.setVisible(true);
     }
 }
